d30ef0314fffa081d0cd9167709537e5f8a886ce
Vítor <vitornsp@al.insper.edu.br>
2022-11-03 15:22:45 -0300

    Transforma base completa

--- Transforma_base_de_questoes.py
+++ Transforma_base_de_questoes.py
@@ -1,3 +1,8 @@
-def transforma_base (questoes):
-    dicionario = {}
-    for questao in questoes: 
+def transforma_base (entrada): 
+    saida = {}
+    for item in entrada:
+        nivel = item['nivel']
+        if nivel not in saida:
+            saida[nivel] = []
+        saida[nivel].append(item)
+    return saida
